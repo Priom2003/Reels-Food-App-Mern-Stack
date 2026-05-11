3bc3738d2c144ec4af5ae6b05e0a8e2c01e889e8
Priom <priom.duttaiembca2024@gmail.com>
2026-05-12 00:52:44 +0530

    Normal

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: '//https://reels-food-app-mern-stack-xqkh.vercel.app/',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(cookieParser());
