5b25a5b41cfcab34700e2f20c3bb462c46cc8f21
Priom <priom.duttaiembca2024@gmail.com>
2026-05-12 00:12:16 +0530

    updated backend for prod

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,13 @@ async function registerUser(req, res) {
         id: user._id,
     },process.env.JWT_SECRET)
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+
+        secure: true,
+
+        sameSite: "none"
+    });
 
     res.status(201).json({
         message: "User registered succesfully",
@@ -67,7 +73,13 @@ async function loginUser(req, res) {
         id: user._id,
     },process.env.JWT_SECRET)
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+
+        secure: true,
+
+        sameSite: "none"
+    });
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -114,7 +126,13 @@ async function registerFoodPartner(req, res){
         id: foodPartner._id,
     },process.env.JWT_SECRET)
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+
+        secure: true,
+
+        sameSite: "none"
+    });
 
     res.status(201).json({
         message: "Food partner registered successfully",
@@ -154,7 +172,11 @@ async function loginFoodPartner(req, res){
         id: foodPartner._id,
     },process.env.JWT_SECRET)
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    });
 
     res.status(200).json({
         message: "Food partner logged in successfully",
